1f615aef377d83961f4e55c28ba78ced4f96042f
He Wang <wanghechn@qq.com>
2024-04-03 12:00:34 +0800

    update ci config

--- tests/sql/test.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `t_test`;
-CREATE TABLE `t_test` (
-    `id`   int(10) NOT NULL AUTO_INCREMENT,
-    `name` varchar(20) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE = utf8_bin;
-
-INSERT INTO `t_test` VALUES (default, 'Hello OceanBase');
