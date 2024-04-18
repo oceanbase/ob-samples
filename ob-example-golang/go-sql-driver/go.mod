2ca1adbd78f2ed2100fed018b75e4b682c97405e
longdafeng <hustjackie@gmail.com>
2024-04-18 16:04:08 +0800

    Reorganize the source code tree

--- ob-example-golang/go-sql-driver/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/oceanbase/ob-example/golang-go-sql-driver
-
-go 1.19
-
-require github.com/go-sql-driver/mysql v1.7.0
--- ob-example-java/oceanbase-client/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-mvn clean install exec:java -Dexec.cleanupDaemonThreads=false -Dexec.mainClass=com.oceanbase.example.InsertAndSelectExample
-mvn clean install exec:java -Dexec.cleanupDaemonThreads=false -Dexec.mainClass=com.oceanbase.example.OceanBaseClientTest
