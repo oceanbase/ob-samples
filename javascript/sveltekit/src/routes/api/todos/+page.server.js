64197f6c8c54fbe3a0d99ed191c845be1d98d39e
榆关 <1170620653@qq.com>
2024-05-17 15:36:55 +0800

    chore: fix sveltekit deployment and add instruction in application readme  (#42)
    * feat: patch js
    
    * feat: patch js
    
    * feat: update db utils
    
    * feat: update readme

---
+++ javascript/sveltekit/src/routes/api/todos/+page.server.js
@@ -0,0 +1,29 @@
+import { createOceanbaseConnection } from '../../../lib/db';
+
+export async function post() {
+  let results = await createOceanbaseConnection()
+    .query('SELECT * FROM tasks')
+    .then(function ([rows, fields]) {
+      console.log(rows);
+      return rows;
+    });
+
+  return {
+    body: results,
+  };
+}
+export async function post(request) {
+  console.log(request);
+  const { text, completed } = request.body;
+  let newTask = await createOceanbaseConnection()
+    .query('INSERT INTO tasks (text, completed) VALUES (?, ?)', [
+      text,
+      completed,
+    ])
+    .then(function ([result]) {
+      return { id: result.insertId, text: text, completed: completed };
+    });
+  return {
+    body: newTask,
+  };
+}
--- javascript/sveltekit/src/routes/api/todos/[id].js
+++ javascript/sveltekit/src/routes/api/todos/[id].js
@@ -1,47 +1,49 @@
-import { oceanbaseConnection } from '../../../lib/db/oceanbase';
+import { createOceanbaseConnection } from '../../../lib/db';
 
 export async function del(req) {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-        await oceanbaseConnection.query('DELETE FROM tasks WHERE id = ?', [id]);
-        return {
-            status: 200,
-            body: { message: `Todo with id ${id} deleted successfully.` },
-        };
-    } catch (error) {
-        console.error('Error deleting todo:', error);
-        return {
-            status: 500,
-            body: { message: 'Error deleting todo. Please try again later.' },
-        };
-    }
+  try {
+    await createOceanbaseConnection().query('DELETE FROM tasks WHERE id = ?', [
+      id,
+    ]);
+    return {
+      status: 200,
+      body: { message: `Todo with id ${id} deleted successfully.` },
+    };
+  } catch (error) {
+    console.error('Error deleting todo:', error);
+    return {
+      status: 500,
+      body: { message: 'Error deleting todo. Please try again later.' },
+    };
+  }
 }
 export async function put(req) {
-    const { id } = req.params;
-    const { completed } = req.body;
+  const { id } = req.params;
+  const { completed } = req.body;
 
-    try {
-        await oceanbaseConnection.query(
-            'UPDATE tasks SET completed = ? WHERE id = ?',
-            [completed, id]
-        );
-        const [updatedTodo] = await oceanbaseConnection.query(
-            'SELECT * FROM tasks WHERE id = ?',
-            [id]
-        );
-        return {
-            status: 200,
-            body: JSON.stringify(updatedTodo),
-        };
-    } catch (error) {
-        console.error('Error toggling todo completed status:', error);
-        return {
-            status: 500,
-            body: {
-                message:
-                    'Error toggling todo completed status. Please try again later.',
-            },
-        };
-    }
+  try {
+    await createOceanbaseConnection().query(
+      'UPDATE tasks SET completed = ? WHERE id = ?',
+      [completed, id],
+    );
+    const [updatedTodo] = await createOceanbaseConnection().query(
+      'SELECT * FROM tasks WHERE id = ?',
+      [id],
+    );
+    return {
+      status: 200,
+      body: JSON.stringify(updatedTodo),
+    };
+  } catch (error) {
+    console.error('Error toggling todo completed status:', error);
+    return {
+      status: 500,
+      body: {
+        message:
+          'Error toggling todo completed status. Please try again later.',
+      },
+    };
+  }
 }
