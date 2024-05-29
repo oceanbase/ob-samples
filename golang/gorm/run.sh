f8660a6a93b531f135f010e2b1533bdaa4f4abd3
yyt030 <yyt030@gmail.com>
2024-05-29 22:28:04 +0800

    chore: update run.sh for ci

--- golang/gorm/run.sh
+++ golang/gorm/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd `dirname $0`
-CGO_ENABLED=0 GO111MODULE=on go run example.go -host 127.0.0.1 -port 3306 -username user@tenant#cluster -password pwd -database dbname
+CGO_ENABLED=0 GO111MODULE=on go run example.go -host 127.0.0.1 -port 2881 -username root@test -database test
