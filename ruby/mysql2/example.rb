7408a2f286055b3a625b59e9f831307f32023539
zhs722 <90595458+zhs722@users.noreply.github.com>
2024-05-30 23:07:44 +0800

    feat: add Ruby mysql2 sample (#61)

---
+++ ruby/mysql2/example.rb
@@ -0,0 +1,41 @@
+require 'mysql2'
+
+def connect_to_oceanbase(host, port, username, password, database)
+  begin
+    # 创建数据库连接
+    client = Mysql2::Client.new(
+      host: host,
+      port: port,
+      username: username,
+      password: password,
+      database: database
+    )
+
+    puts "连接到OceanBase数据库成功"
+
+    # 执行一个简单的查询
+    results = client.query("SELECT DATABASE();")
+
+    results.each do |row|
+      puts "连接到的数据库: #{row['DATABASE()']}"
+    end
+
+    # 你可以在这里执行其他的SQL查询或操作
+
+    client.close
+    puts "MySQL连接已关闭"
+
+  rescue Mysql2::Error => e
+    puts "连接到OceanBase数据库失败: #{e.error}"
+  end
+end
+
+if __FILE__ == $0
+  connect_to_oceanbase(
+    '127.0.0.1',
+    2881,
+    'root',
+    '',
+    'test'
+  )
+end
